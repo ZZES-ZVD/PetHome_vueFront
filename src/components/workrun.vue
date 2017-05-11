b9fab70d67aeaad6c38fa9410140b79f8108299f
zzes <kuanglong0312@sina.com>
2017-05-11 21:35:36 +0800

    工作流程页面

---
+++ src/components/workrun.vue
@@ -0,0 +1,21 @@
+<template>
+	<el-row>
+	  <el-col :span="24"    >
+	    <el-card :body-style="{ padding: '0px' }">
+	      <img src="../assets/workrun.jpg" id="image1" >
+ 
+	    </el-card>
+	  </el-col>
+	</el-row>
+ 
+</template>
+
+<style>
+	#image1{
+		width: 100%;
+	}
+</style>
+
+<script>
+	
+</script>
