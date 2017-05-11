eeadf7445a6409fa9e05edc9ffc2eede721dc87d
zzes <kuanglong0312@sina.com>
2017-05-11 21:35:51 +0800

    入口文件

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import App from './App.vue'
 const Index = resolve => {require.ensure([],() => {resolve(require('./components/index.vue'))})};
 const Home = resolve => {require.ensure([], () => {resolve(require('./components/home.vue'))})};
 const Status = resolve => {require.ensure([], () => {resolve(require('./components/status.vue'))})};
-
+const Hardware = resolve => {require.ensure([], () => {resolve(require('./components/hardware.vue'))})};
+const Software = resolve => {require.ensure([], () => {resolve(require('./components/software.vue'))})};
+const Workdoc = resolve => {require.ensure([], () => {resolve(require('./components/workdoc.vue'))})};
+const Workrun = resolve => {require.ensure([], () => {resolve(require('./components/workrun.vue'))})};
+const Aboutus = resolve => {require.ensure([], ()=> {resolve(require('./components/aboutus.vue'))})};
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
@@ -16,7 +20,12 @@ Vue.use(VueRouter)
 const routes = [
 	{path:'/',component:Index},
 	{path:'/home',component:Home},
-	{path:'/status',component:Status}
+	{path:'/status',component:Status},
+	{path:'/hardware',component:Hardware},
+	{path:'/software',component:Software},
+	{path:'/workdoc',component:Workdoc},
+	{path:'/workrun',component:Workrun},
+	{path:'/aboutus',component:Aboutus}
 ]
 
 const router = new VueRouter({
